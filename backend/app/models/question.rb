e693d16749a72292668d741b4697a9db9f9de198
Helen Hood <helen.c.hood@gmail.com>
2014-08-07 12:53:18 -0400

    Validate that accepted answer belongs to right question

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -8,4 +8,11 @@ class Question < ActiveRecord::Base
   validates :body, length: { in: 30..10000 }
   validates :title, length: { in: 15..150 }
   validates :user, presence: true
+  validate :accepted_answer_belongs_to_question
+
+  def accepted_answer_belongs_to_question
+    if accepted_answer && accepted_answer.question != self
+      errors.add(:accepted_answer, "must be in response to this question")
+    end
+  end
 end
--- backend/spec/models/question_spec.rb
+++ backend/spec/models/question_spec.rb
@@ -12,5 +12,22 @@ describe Question do
     it { should ensure_length_of(:body).is_at_least(30).is_at_most(10000) }
     it { should ensure_length_of(:title).is_at_least(15).is_at_most(150) }
     it { should validate_presence_of(:user) }
+
+    it "should allow an accepted answer that belongs to the right question" do
+      answer = FactoryGirl.build_stubbed(:answer)
+      question = answer.question
+      question.accepted_answer = answer
+
+      expect(question).to be_valid
+    end
+
+    it "should not allow an accepted answer if it belongs to a different question" do
+      answer = FactoryGirl.build_stubbed(:answer)
+      question = FactoryGirl.build_stubbed(:question)
+      question.accepted_answer = answer
+
+      expect(question).to_not be_valid
+      expect(question.errors.full_messages).to include "Accepted answer must be in response to this question"
+    end
   end
 end
