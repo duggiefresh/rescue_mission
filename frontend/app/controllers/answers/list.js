7c0d4679a048d54a93550b950cb40eb90078d73e
Helen Hood <helen.c.hood@gmail.com>
2014-08-07 14:36:07 -0400

    Question show template displays just unselected answers

--- frontend/app/controllers/answers/list.js
+++ frontend/app/controllers/answers/list.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  itemController: 'answers/list-item'
+  itemController: 'answers/list-item',
+
+  unselectedAnswers: function() {
+    return this.get('model').filterProperty('isAcceptedAnswer', false);
+  }.property('model.@each.isAcceptedAnswer'),
 });
